3b711ee8514c019adbc1b5b6b62e7f9b21c43a48
sajicode <sajioloye@gmail.com>
2020-04-22 06:16:33 +0100

    add build folder to git

---
+++ client/build/precache-manifest.05372577cae2f1ad37b7205ca330a443.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e5ff802821a0acf05cfca8fb56fe3a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c937dc8230fea76ac0af",
+    "url": "./static/css/main.fd44b7fc.chunk.css"
+  },
+  {
+    "revision": "98cdca4ff16834c23e32",
+    "url": "./static/js/2.9a43cd51.chunk.js"
+  },
+  {
+    "revision": "00cd77c8a79ad22a2fedb5fcb9877770",
+    "url": "./static/js/2.9a43cd51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c937dc8230fea76ac0af",
+    "url": "./static/js/main.cc3f9165.chunk.js"
+  },
+  {
+    "revision": "1e8c7afc7bf36ad0e5f1",
+    "url": "./static/js/runtime-main.64664b1b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
